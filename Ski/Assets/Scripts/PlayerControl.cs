d2418fad137038dd0008457fc8c5be70e2831d5a
JulyCat6 <fluttershout1029@gmail.com>
2026-04-13 15:44:47 +0300

    player moves and Background

---
+++ Ski/Assets/Scripts/PlayerControl.cs
@@ -0,0 +1,30 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerControl : MonoBehaviour
+{
+    private InputAction move;
+    [SerializeField] private float rotSpeed = 30, speed = 20;
+    private Rigidbody rb;
+    
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Awake()
+    {
+        move = InputSystem.actions.FindAction("Player/Move"); 
+        rb = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        
+        //transform.forward = move.ReadValue<Vector2>();
+        Vector2 moveInput = move.ReadValue<Vector2>();
+        Debug.Log("x: " + moveInput.x + " y: " + moveInput.y);
+        transform.Rotate(0, -moveInput.x * rotSpeed * Time.fixedDeltaTime, 0 );
+        float turnAngle = Mathf.Abs(180 - transform.localEulerAngles.y );
+        float speedMult = Mathf.Cos(turnAngle * Mathf.Deg2Rad); //Rad - radiant
+        rb.AddForce(transform.forward * speed * speedMult * Time.fixedDeltaTime);
+        Debug.Log(" turnAngle: " + turnAngle );
+    }
+}
